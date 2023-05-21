7617159168f6eb42852b263cb494a3d51c7603ee
sqrtleet <sqrtleet@gmail.com>
2023-05-21 23:27:02 +0900

    mongo.py server

--- resources/potholes.py
+++ resources/potholes.py
@@ -7,16 +7,14 @@ app = FastAPI()
 
 @app.get("/potholes")
 async def potholes():
-    results = list(col_potholes.find({}, {"_id": 1, "img_detect": 1, "geo": 1, "potholes_count": 1}))
+    results = list(col_potholes.find({}, {"_id": 1, "geo": 1}))
     geo_data = []
 
     for result in results:
         latitude = result["geo"][0]
         longitude = result["geo"][1]
         obj_id = str(result["_id"])
-        img = result["img_detect"]
-        count = result["potholes_count"]
         geo_data.append(
-            {"id": obj_id, "latitude": latitude, "longitude": longitude, "img": img, "potholes_count": count})
+            {"id": obj_id, "latitude": latitude, "longitude": longitude})
 
     return JSONResponse(geo_data)
